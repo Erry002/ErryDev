6a821c754180bddd2dfbc70596631969cc396c1d
Erry002 <enricogarozzo002@gmail.com>
2024-03-22 22:21:11 +0100

    fix card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,17 +6,19 @@ export default function Card({
   imageUrl,
   urlGit,
   urlSito,
+  btnTesto1,
+  btnTesto2,
 }) {
   const handleButtonClick = (url) => {
     window.open(url, "_blank");
   };
 
   return (
-    <div className=" gap-10 h-[620px] sm:w-[350px] flex flex-col items-center rounded-lg m-2 bg-blue-900 overflow-hidden hover:bg-blue-800 hover:scale-105 transition">
+    <div className=" gap-10 h-[620px] sm:w-[350px] flex flex-col items-center rounded-lg m-4 bg-blue-900 overflow-hidden hover:bg-blue-800 hover:scale-105 transition">
       <img className="w-full" src={imageUrl} alt="Card" />
-      <div className="px-6 py-0">
+      <div className="px-6 -mb-4 sm:h-[250px]">
         <div className="font-bold text-blue-300 text-2xl mb-2">{title}</div>
-        <p className="text-slate-100 text-base">{description}</p>
+        <p className="text-slate-100 h-full text-base">{description}</p>
       </div>
       <div className="px-6 py-7 w-full flex flex-col justify-end items-end">
         <button
@@ -28,7 +30,7 @@ export default function Card({
             )
           }
         >
-          Apri su GitHub
+          {btnTesto1}
         </button>
         <button
           className="bg-red-700 w-full p-2 mb-4 rounded-xl transition-all hover:underline hover:bg-red-600 underline-offset-4 mt-auto"
@@ -39,9 +41,55 @@ export default function Card({
             )
           }
         >
-          Provalo subito!
+          {btnTesto2}
         </button>
       </div>
     </div>
   );
 }
+
+
+/* 
+//Progetto di card non convertito in componente!
+
+            <div className="h-[620px] sm:w-[350px] flex flex-col items-center rounded-lg m-2 bg-blue-900 overflow-hidden hover:bg-blue-800 hover:scale-105 transition ">
+              <div className="">
+                <img
+                  src={logo}
+                  alt="logo_card_landing"
+                  className="rounded-lg shadow-xl my-2 w-full"
+                />
+              </div>
+              <div className="p-5">
+                <h1 className="mt-5 text-xl text-blue-300">
+                  Un esempio di landing page personalizzata
+                </h1>
+                <p className="mt-5 text-slate-200 mb-6">
+                  Questo è un esempio di landing page di una agenzia di viaggi,
+                  completa anche di form per le richieste, completamente
+                  responsive. <br />
+                  Clicca il bottone per vedere il codice su GitHub.
+                </p>
+                <button
+                  className="bg-blue-700 w-full p-2 mb-4 rounded-xl transition-all hover:underline hover:bg-blue-600 underline-offset-4 mt-auto"
+                  onClick={() =>
+                    handleButtonClick(
+                      "https://github.com/Erry002/simulazione-landing/tree/main/code"
+                    )
+                  }
+                >
+                  Apri su GitHub
+                </button>
+                <Link
+                  target="_blank" rel="noopener noreferrer"
+                  to="https://erryairlines.netlify.app/"
+                >
+                  <button
+                  className="bg-blue-700 w-full p-2 mb-4 rounded-xl transition-all hover:underline hover:bg-blue-600 underline-offset-4 mt-auto"
+                  >
+                    Provalo Subito!
+                  </button>
+                </Link>
+              </div>
+            </div> 
+*/
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -27,153 +27,35 @@ export default function Project() {
               imageUrl={logo}
               urlGit="https://github.com/Erry002/simulazione-landing/tree/main/code"
               urlSito="https://erryairlines.netlify.app/"
+              btnTesto1="Apri su GitHub"
+              btnTesto2="Provalo subito"
             />
-            {/* <div className="h-[620px] sm:w-[350px] flex flex-col items-center rounded-lg m-2 bg-blue-900 overflow-hidden hover:bg-blue-800 hover:scale-105 transition ">
-              <div className="">
-                <img
-                  src={logo}
-                  alt="logo_card_landing"
-                  className="rounded-lg shadow-xl my-2 w-full"
-                />
-              </div>
-              <div className="p-5">
-                <h1 className="mt-5 text-xl text-blue-300">
-                  Un esempio di landing page personalizzata
-                </h1>
-                <p className="mt-5 text-slate-200 mb-6">
-                  Questo è un esempio di landing page di una agenzia di viaggi,
-                  completa anche di form per le richieste, completamente
-                  responsive. <br />
-                  Clicca il bottone per vedere il codice su GitHub.
-                </p>
-                <button
-                  className="bg-blue-700 w-full p-2 mb-4 rounded-xl transition-all hover:underline hover:bg-blue-600 underline-offset-4 mt-auto"
-                  onClick={() =>
-                    handleButtonClick(
-                      "https://github.com/Erry002/simulazione-landing/tree/main/code"
-                    )
-                  }
-                >
-                  Apri su GitHub
-                </button>
-                <Link
-                  target="_blank" rel="noopener noreferrer"
-                  to="https://erryairlines.netlify.app/"
-                >
-                  <button
-                  className="bg-blue-700 w-full p-2 mb-4 rounded-xl transition-all hover:underline hover:bg-blue-600 underline-offset-4 mt-auto"
-                  >
-                    Provalo Subito!
-                  </button>
-                </Link>
-              </div>
-            </div> */}
 
             {/* seconda card */}
             <Card
-              title="Landing page"
-              description="Questo è un esempio di landing page di una agenzia di viaggi, completa anche di form per le richieste, completamente
-        responsive. Clicca il bottone per vedere il codice su GitHub."
+              title="Piccolo progetto personale con API"
+              description="Questo è un esempio è composto da altri piccoli progetti
+              personali che ho fatto durante un corso di programmazione, in questo mostro come utilizzare le API"
               imageUrl={fatcs}
               urlGit="https://github.com/Erry002/Cartella-documentazione-corsi-WebDev/blob/main/Corso%20Opinno%20WebDev/Programma%20corso%20Opinno/Giorno_21_introduzione_react/Progetti_con_React_%2B_Vite/progetto_con_vit/doc_amo/src/EsFect.jsx"
               urlSito="https://errydev.netlify.app/catFact"
+              btnTesto1="Apri su GitHub"
+              btnTesto2="Provalo subito"
             />
-            {/* <div className="h-[620px] sm:w-[350px] flex flex-col items-center rounded-lg m-2 bg-blue-900 overflow-hidden hover:bg-blue-800 hover:scale-105 transition ">
-              <div className=" ">
-                <img
-                  src={fatcs}
-                  alt="logo_card_cat_facts"
-                  className="rounded-lg shadow-xl my-2 w-full"
-                />
-              </div>
-              <div className="w-full px-5 flex flex-col justify-between">
-                <div>
-                  <h1 className="mt-5 text-xl text-blue-300">
-                    Piccolo progetto personale con API
-                  </h1>
-                  <p className="mt-5 text-slate-200 mb-6">
-                    Questo è un esempio è composto da altri piccoli progetti
-                    personali che ho fatto, in questo mostro come utilizzare le
-                    API
-                    <br />
-                    Clicca il bottone per vedere il codice su GitHub.
-                  </p>
-                </div>
-                <div className="">
-                  <button
-                    className="bg-blue-700 w-full p-2 rounded-xl sm:mt-7 transition-all hover:underline hover:bg-blue-600 underline-offset-4 "
-                    onClick={() =>
-                      handleButtonClick(
-                        "https://github.com/Erry002/Cartella-documentazione-corsi-WebDev/blob/main/Corso%20Opinno%20WebDev/Programma%20corso%20Opinno/Giorno_21_introduzione_react/Progetti_con_React_%2B_Vite/progetto_con_vit/doc_amo/src/EsFect.jsx"
-                      )
-                    }
-                  >
-                    Apri su GitHub
-                  </button>
-
-                  <Link to="/catFact">
-                    <button className="bg-blue-700 w-full p-2 rounded-xl sm:mt-4 mt-4 transition-all hover:underline hover:bg-blue-600 underline-offset-4 ">
-                      Provalo Subito!
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            </div> */}
 
             {/* terza card */}
             <Card
-              title="Landing page"
-              description="Questo è un esempio di landing page di una agenzia di viaggi, completa anche di form per le richieste, completamente
-        responsive. Clicca il bottone per vedere il codice su GitHub."
+              title="Progetto di gruppo di un sito di giochi"
+              description="Questo progetto rappresenta un'opera collaborativa, nel
+              quale ho contribuito non solo alla realizzazione della
+              pagina iniziale, ma anche alla creazione del gioco
+              denominato 'Reaction Time'."
               imageUrl={games}
               urlGit="https://github.com/Erry002/Cartella-documentazione-corsi-WebDev/tree/main/Corso%20Opinno%20WebDev/Programma%20corso%20Opinno/Lavoro_gruppo_dicembre_Sito_giochi"
               urlSito="https://siteplaygame.netlify.app/"
+              btnTesto1="Apri su GitHub"
+              btnTesto2="Provalo subito"
             />
-            {/* <div className="h-[620px] sm:w-[350px] flex flex-col items-center rounded-lg m-2 bg-blue-900 overflow-hidden hover:bg-blue-800 hover:scale-105 transition ">
-              <div>
-                <img
-                  src={games}
-                  alt="logo_card_games"
-                  className="rounded-lg shadow-xl my-2 w-full"
-                />
-              </div>
-              <div className="w-full px-5 flex flex-col justify-between">
-                <div>
-                  <h1 className="mt-5 text-xl text-blue-300">
-                    Progetto di gruppo di un sito di giochi
-                  </h1>
-                  <p className="mt-5 text-slate-200 mb-9">
-                    Questo progetto rappresenta un'opera collaborativa, nel
-                    quale ho contribuito non solo alla realizzazione della
-                    pagina iniziale, ma anche alla creazione del gioco
-                    denominato 'Reaction Time'.
-                    <br />
-                    Clicca il bottone per vedere il codice su GitHub.
-                  </p>
-                </div>
-                <div className="">
-                  <button
-                    className="bg-blue-700 w-full p-2 mb-4 rounded-xl transition-all hover:underline hover:bg-blue-600 underline-offset-4 mt-[-13px]"
-                    onClick={() =>
-                      handleButtonClick(
-                        "https://github.com/Erry002/Cartella-documentazione-corsi-WebDev/tree/main/Corso%20Opinno%20WebDev/Programma%20corso%20Opinno/Lavoro_gruppo_dicembre_Sito_giochi"
-                      )
-                    }
-                  >
-                    Apri su GitHub
-                  </button>
-                  <Link
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    to="https://siteplaygame.netlify.app/"
-                  >
-                    <button className="bg-blue-700 w-full p-2 rounded-xl transition-all hover:underline hover:bg-blue-600 underline-offset-4">
-                      Provalo Subito!
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            </div> */}
           </div>
         </div>
       </div>
